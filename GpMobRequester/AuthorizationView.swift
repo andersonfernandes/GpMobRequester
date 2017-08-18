30c36eef0e5ceae2fc2a38e88e631b0e412e4b2e
Mr.Jones <jones@rogalabs.com>
2017-08-18 17:05:27 -0300

    #finalized-login-view-skeleton
    Signed-off-by: Mr.Jones <jones@rogalabs.com>

---
+++ GpMobRequester/AuthorizationView.swift
@@ -0,0 +1,16 @@
+//
+//  AuthorizationView.swift
+//  GpMobRequester
+//
+//  Created by roga on 8/18/17.
+//  Copyright © 2017 Stant. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AuthorizationView: UIView {
+    
+    @IBOutlet weak var btnSendAutorization: UIButton!
+    @IBOutlet weak var inputAutorizationCode: UITextField!
+}
---
+++ GpMobRequester/AuthorizationViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AuthorizationViewController.swift
+//  GpMobRequester
+//
+//  Created by roga on 8/18/17.
+//  Copyright © 2017 Stant. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AuthorizationViewController: UIViewController {
+    
+}
